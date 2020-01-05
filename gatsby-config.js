9e3b0444eb4c8f3dc6afcade69f8f3c345bfd962
Louis <louispierre516@gmail.com>
2020-01-05 15:16:50 -0400

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Louis Blog CMS ',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is an example of a business website blog hosted on Netlify CMS.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
